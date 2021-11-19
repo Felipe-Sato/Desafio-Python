4c99d53d11b27c6c6b7cab22d6cf81aa48793515
Felipe Sato <felipe.sato270@gmail.com>
2021-11-19 11:06:55 -0300

    Update Desafio 1.py
    Otimização da leitura do algoritmo através da redução de comandos.

--- Desafio 1.py
+++ Desafio 1.py
@@ -9,6 +9,6 @@ X = range(2,5000000,2)
 #Loop para verificar o vetor
 for i in X:
     if i % B == 0 and i % C == 0:
-        Contador = Contador + 1
+        Contador += 1
     
 print(Contador) #Impressao da resposta
